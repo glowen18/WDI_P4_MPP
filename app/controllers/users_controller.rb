f9c76a70341c91903c4ef7d5b82b7da21df530b6
Gaye Lowenstein <glowen@hotmail.com>
2016-12-14 18:25:56 -0800

    added project page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(@user_params)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id.to_s
       redirect_to users_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :projects
-
-  def password=(word)
-    self.password_digest = "$2a$10$djkCu..."
-    @password = word
-  end
-
-  def password_confirmation(word)
-    @password_confirmation = word
-  end
 end
